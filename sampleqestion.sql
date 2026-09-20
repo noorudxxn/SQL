ac629ed45cd8f3afaee3de5c0dd47b0356d2157a
noorudeen <noorudeen2004@gmail.com>
2026-09-20 11:54:45 +0530

    Add initial SQL queries for student data manipulation

---
+++ sampleqestion.sql
@@ -0,0 +1,8 @@
+update students set course = "web development" WHERE course =  "html" or course =" css" and not place = goa 
+SELECT * from students where studentis<5 
+update student set place = kerala where course = java and plave =trivanrum or malapuram ;
+deletee students courde css place punjb , mumbai course =d jango 
+delete from students where course = c++ and punjab   or course djangob and plave mumbai 
+select * from student where courde <> c++   ORDERBY     studentid desc ,place asc;
+displa name age course place xno not tr=vm older that 19,stdy java or python ,then arrange reslut age asc
+    select name, age , course , place from students where place <> trivandrum  , age > 19 , course =  java or pythin  order by age asc;
